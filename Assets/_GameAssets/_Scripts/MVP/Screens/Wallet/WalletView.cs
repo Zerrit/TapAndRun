ee877ba4d22a9288d657ebcc1267f2c712671ce6
Ruslan <rus.ust@mail.ru>
2024-08-13 11:06:07 +0300

    Добавлена анимация изменения числа кристалов.

--- Assets/_GameAssets/_Scripts/MVP/Screens/Wallet/WalletView.cs
+++ Assets/_GameAssets/_Scripts/MVP/Screens/Wallet/WalletView.cs
@@ -1,4 +1,7 @@
-﻿using TMPro;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace TapAndRun.MVP.Screens.Wallet
@@ -8,11 +11,13 @@ namespace TapAndRun.MVP.Screens.Wallet
         [field:SerializeField] public TextMeshProUGUI AvailableCrystals { get; private set; }
         [field:SerializeField] public TextMeshProUGUI CrystalsByLevel { get; private set; }
 
+        private CancellationTokenSource _cts = new ();
+
         public void UpdateAvailableCrystals(int newValue)
         {
             AvailableCrystals.text = newValue.ToString();
         }
-        
+
         public void UpdateCrystalsByLevel(int newValue)
         {
             if (newValue <= 0)
@@ -22,6 +27,23 @@ namespace TapAndRun.MVP.Screens.Wallet
             }
 
             CrystalsByLevel.text = $" +{newValue}";
+
+            ResetTokenSource();
+            PlayAnimation(_cts.Token).Forget();
+        }
+
+        private async UniTaskVoid PlayAnimation(CancellationToken token)
+        {
+            await CrystalsByLevel.transform.DOScale(new Vector2(1.3f, 1.3f), 0.1f)
+                .SetLoops(2, LoopType.Yoyo)
+                .AwaitForComplete(TweenCancelBehaviour.CompleteAndCancelAwait, token);
+        }
+
+        private void ResetTokenSource()
+        {
+            _cts.Cancel();
+            _cts.Dispose();
+            _cts = new CancellationTokenSource();
         }
     }
 }
