28f9c38f1091704e67d53bedd6803f1becdce53b
Ruslan <rus.ust@mail.ru>
2024-09-11 14:22:17 +0300

    Добавлен атлас и внесены мелкие правки

--- Assets/_GameAssets/_Scripts/Services/Audio/IAudioService.cs
+++ Assets/_GameAssets/_Scripts/Services/Audio/IAudioService.cs
@@ -1,10 +1,13 @@
-﻿using TapAndRun.Interfaces;
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using TapAndRun.Interfaces;
 
 namespace TapAndRun.Services.Audio
 {
     public interface IAudioService: IInitializableAsync
     {
         void PlaySound(string id);
+        UniTask PlaySoundAsync(string id, float delayTime, CancellationToken token);
         void CallVibration();
     }
 }
